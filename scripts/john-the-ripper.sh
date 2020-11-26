a2c053f92082d7c1abad04f0d953b2a580f81e64
Laik4 <laika@albina.cc>
2020-11-26 21:58:38 +0900

    Rename a variable

--- scripts/john-the-ripper.sh
+++ scripts/john-the-ripper.sh
@@ -2,23 +2,23 @@
 
 TMPDIR="${TMPDIR:-/tmp}"
 INIT_PATH="$(pwd)"
-TOOL_PATH="${HOME}/CTF/Tools"
+TOOL_DIR="${HOME}/CTF/Tools"
 
 
 sudo apt-get update -y
 sudo apt-get install -y git build-essential libssl-dev zlib1g-dev yasm pkg-config libgmp-dev libpcap-dev libbz2-dev
 
 
-if [ ! -e ${TOOL_PATH}/john ]; then
-    git clone https://github.com/magnumripper/JohnTheRipper -b bleeding-jumbo ${TOOL_PATH}/john
+if [ ! -e ${TOOL_DIR}/john ]; then
+    git clone https://github.com/magnumripper/JohnTheRipper -b bleeding-jumbo ${TOOL_DIR}/john
 fi
 
-cd ${TOOL_PATH}/john/src
+cd ${TOOL_DIR}/john/src
 
 ./configure && make -s clean 
 make -j$(nproc)
 
-sudo ln -sf ${TOOL_PATH}/john/run/john /usr/bin/john
-sudo ln -sf ${TOOL_PATH}/john/run/zip2john /usr/bin/zip2john
+sudo ln -sf ${TOOL_DIR}/john/run/john /usr/bin/john
+sudo ln -sf ${TOOL_DIR}/john/run/zip2john /usr/bin/zip2john
 
 cd ${INIT_PATH}
