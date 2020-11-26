92ed8697ec2e968b3677281b8e63f9311d91dc44
Laik4 <laika@albina.cc>
2020-11-26 21:57:37 +0900

    Update john the ripper

--- scripts/john-the-ripper.sh
+++ scripts/john-the-ripper.sh
@@ -1,24 +1,24 @@
 #!/bin/bash -e 
 
+TMPDIR="${TMPDIR:-/tmp}"
+INIT_PATH="$(pwd)"
+TOOL_PATH="${HOME}/CTF/Tools"
 
-echo [+] Install requirements
+
 sudo apt-get update -y
 sudo apt-get install -y git build-essential libssl-dev zlib1g-dev yasm pkg-config libgmp-dev libpcap-dev libbz2-dev
 
-echo [+] Clone from GitHub
-if [ ! -e /tmp/john ]; then
-    git clone https://github.com/magnumripper/JohnTheRipper -b bleeding-jumbo /tmp/john
+
+if [ ! -e ${TOOL_PATH}/john ]; then
+    git clone https://github.com/magnumripper/JohnTheRipper -b bleeding-jumbo ${TOOL_PATH}/john
 fi
 
-pushd /tmp/john/src
+cd ${TOOL_PATH}/john/src
 
-echo [+] Start building
-./configure && make -s clean && make -j$(nproc)
+./configure && make -s clean 
+make -j$(nproc)
 
-echo [+] Make symbolic links
-sudo mv $JOHNPATH/john/run/john /usr/bin/john
-sudo mv $JOHNPATH/john/run/zip2john /usr/bin/zip2john
+sudo ln -sf ${TOOL_PATH}/john/run/john /usr/bin/john
+sudo ln -sf ${TOOL_PATH}/john/run/zip2john /usr/bin/zip2john
 
-
-popd
-echo [+] Done
+cd ${INIT_PATH}
