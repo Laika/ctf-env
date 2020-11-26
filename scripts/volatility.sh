12ae86107f28747e4bdfe85ae500ce0950ebf611
Laik4 <laika@albina.cc>
2020-11-26 21:58:24 +0900

    Update volatility

--- scripts/volatility.sh
+++ scripts/volatility.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+TMPDIR=${TMPDIR:-/tmp}
+INIT_PATH=$(pwd)
+
 
 sudo apt-get update -y
 sudo apt-get install -y unzip
 # volatility
-if [ ! -e /tmp/volatility_2.6_lin64_standalone.zip ]; then
-  wget -q -O /tmp/volatility_2.6_lin64_standalone.zip http://downloads.volatilityfoundation.org/releases/2.6/volatility_2.6_lin64_standalone.zip
+if [ ! -e ${TMPDIR}/volatility_2.6_lin64_standalone.zip ]; then
+  wget -q -O ${TMPDIR}/volatility_2.6_lin64_standalone.zip http://downloads.volatilityfoundation.org/releases/2.6/volatility_2.6_lin64_standalone.zip
 fi
 
-cd /tmp
+cd ${TMPDIR}
 unzip -q -o volatility_2.6_lin64_standalone.zip
 sudo mv -f volatility_2.6_lin64_standalone/volatility_2.6_lin64_standalone /usr/local/bin/volatility
+
+cd ${INIT_PATH}
