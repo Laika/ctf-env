216d61d35bf22d52c19799be26d00178f0788c59
Laik4 <laika@albina.cc>
2020-11-26 21:58:45 +0900

    Update radare2

--- scripts/radare2.sh
+++ scripts/radare2.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 TMPDIR=${TMPDIR:-/tmp}
+TOOL_DIR="${HOME}/CTF/Tools"
+INIT_PATH="$(pwd)"
 
-sudo apt-get update -y
-sudo apt-get install -y ccache cmake bison flex
-if [ ! -e /tmp/radare2 ]; then
-  git clone https://github.com/radareorg/radare2.git ${TMPDIR}/radare2 
+if [ ! -e ${TOOL_DIR}/radare2 ]; then
+  sudo apt-get update -y
+  sudo apt-get install -y ccache cmake bison flex
+  git clone https://github.com/radareorg/radare2.git ${TOOL_DIR}/radare2 
 fi
-cd /tmp/radare2
-sudo sys/install.sh --install
-sudo rm -rf /tmp/*
 
-r2pm init
-r2pm update
-r2pm -i r2ghidra-dec
+cd ${TOOL_DIR}/radare2
+sudo sys/install.sh --install
+
+if type "r2" > /dev/null 2>&1; then
+  r2pm init
+  r2pm -i r2ghidra-dec
+else
+  r2pm update
+fi
