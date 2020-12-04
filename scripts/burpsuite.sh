18ea5bbc0431893b4a62dceecb444ecaa629a1a2
Laik4 <laika@albina.cc>
2020-12-04 22:33:34 +0900

    Add burpsuite

---
+++ scripts/burpsuite.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+INIT_PATH=$(pwd)
+TMPDIR=${TMPDIR:-/tmp}
+VERSION=2020.11.3
+FILENAME=burpsuite_community_linux_v${VERSION//\./\_}
+
+sudo apt update -y
+sudo apt install -y axel
+
+if [ ! -e ${TMPDIR}/${FILENAME}.sh ]; then
+  axel -n 8 -v \
+    -a "https://portswigger.net/burp/releases/download?product=community&version=${VERSION}&type=Linux" \
+    -o ${TMPDIR}
+fi
+
+cd ${TMPDIR}
+bash ${FILENAME}.sh
+rm ${FILENAME}.sh
+
+cd ${INIT_PATH}
