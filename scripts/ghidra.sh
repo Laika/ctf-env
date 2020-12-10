c693a15b305fb84db82e72c235a4d2a1c51f4c73
Laik4 <laika@albina.cc>
2020-12-10 12:18:57 +0900

    Update ghidra

--- scripts/ghidra.sh
+++ scripts/ghidra.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 
 TMPDIR=${TMPDIR:-/tmp}
@@ -7,9 +7,11 @@ TOOLDIR=${TOOLDIR:-${HOME}/CTF/Tools}
 FILENAME="ghidra_9.2_PUBLIC_20201113.zip"
 
 
-sudo apt update -y && apt install -y openjdk-14-jdk openjdk-14-jre
+sudo apt update -y && sudo apt install -y openjdk-14-jdk
 
-wget "https://ghidra-sre.org/${FILENAME}" -P ${TOOLDIR}
+if [ ! -e ${TOOLDIR}/${FILENAME} ]; then
+  wget "https://ghidra-sre.org/${FILENAME}" -P ${TOOLDIR}
+fi
 cd ${TOOLDIR} && unzip ${FILENAME}
 
 sudo ln -sf ${TOOLDIR}/${FILENAME%_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].zip}/ghidraRun /usr/local/bin/ghidra
