be1202687b50cce7dc9b4c15a141a3bd313a87a1
Laik4 <laika@albina.cc>
2020-12-13 00:04:46 +0900

    Add skip

--- scripts/radare2.sh
+++ scripts/radare2.sh
@@ -4,16 +4,18 @@ TMPDIR=${TMPDIR:-/tmp}
 TOOL_DIR="${HOME}/CTF/Tools"
 INIT_PATH="$(pwd)"
 
-if [ ! -e ${TOOL_DIR}/radare2 ]; then
-  sudo apt-get update -y
-  sudo apt-get install -y ccache cmake bison flex
-  git clone https://github.com/radareorg/radare2.git ${TOOL_DIR}/radare2 
+if [ ! $(command -v r2) ]; then
+  if [ ! -e ${TOOL_DIR}/radare2 ]; then
+    sudo apt-get update -y
+    sudo apt-get install -y ccache cmake bison flex
+    git clone https://github.com/radareorg/radare2.git ${TOOL_DIR}/radare2 
+  fi
 fi
 
 cd ${TOOL_DIR}/radare2
 sudo sys/install.sh --install
 
-if type "r2" > /dev/null 2>&1; then
+if [ ! $(command -v r2) ]; then
   r2pm init
   r2pm -i r2ghidra
 else
