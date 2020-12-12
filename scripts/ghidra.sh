a8e72528912e512639905177ccc7c580876f49d5
Laik4 <laika@albina.cc>
2020-12-13 00:09:23 +0900

    Add skip

--- scripts/ghidra.sh
+++ scripts/ghidra.sh
@@ -7,13 +7,13 @@ TOOLDIR=${TOOLDIR:-${HOME}/CTF/Tools}
 FILENAME="ghidra_9.2_PUBLIC_20201113.zip"
 
 
-sudo apt update -y && sudo apt install -y openjdk-14-jdk
+if [ ! $(command -v ghidra) ]; then 
+  sudo apt update -y && sudo apt install -y openjdk-14-jdk
 
-if [ ! -e ${TOOLDIR}/${FILENAME} ]; then
-  wget "https://ghidra-sre.org/${FILENAME}" -P ${TOOLDIR}
+  if [ ! -e ${TOOLDIR}/${FILENAME} ]; then
+    wget "https://ghidra-sre.org/${FILENAME}" -P ${TOOLDIR}
+  fi
+  cd ${TOOLDIR} && unzip ${FILENAME}
+  sudo ln -sf ${TOOLDIR}/${FILENAME%_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].zip}/ghidraRun /usr/local/bin/ghidra
 fi
-cd ${TOOLDIR} && unzip ${FILENAME}
-
-sudo ln -sf ${TOOLDIR}/${FILENAME%_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].zip}/ghidraRun /usr/local/bin/ghidra
-
 
--- scripts/sage.sh
+++ scripts/sage.sh
@@ -3,14 +3,15 @@
 VER=9.2
 INSTALL_DIR=${HOME}/CTF
 INSTALL_PATH=${INSTALL_DIR:?'${INSTALL_DIR} is not defined'}/Tools
-FILENAME=sage-${VER}-Ubuntu_20.04-x86_64
+FILENAME="sage-${VER}-Ubuntu_20.04-x86_64"
 
+if [ ! $(command -v sage) ]; then
+  if [ ! -e ${INSTALL_PATH}/${FILENAME}.tar.bz2 ];then
+      axel -a "http://ftp.yz.yamagata-u.ac.jp/pub/math/sage/linux/64bit/${FILENAME}.tar.bz2" -o ${INSTALL_PATH}
+  fi
 
-if [ ! -e ${INSTALL_PATH}/${FILENAME}.tar.bz2 ];then
-    axel -a "http://ftp.yz.yamagata-u.ac.jp/pub/math/sage/linux/64bit/${FILENAME}.tar.bz2" -o ${INSTALL_PATH}
+  cd ${INSTALL_PATH}
+  tar xvf ${FILENAME}.tar.bz2
+  sudo ln -sf ${INSTALL_PATH}/SageMath/sage /usr/local/bin/sage
+  rm ${INSTALL_PATH}/${FILENAME}.tar.bz2
 fi
-
-cd ${INSTALL_PATH}
-tar xvf ${FILENAME}.tar.bz2
-sudo ln -sf ${INSTALL_PATH}/SageMath/sage /usr/local/bin/sage
-rm ${INSTALL_PATH}/${FILENAME}.tar.bz2
