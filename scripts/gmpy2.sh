0c4f574511f6bb274fa804ecc555ada543230178
Laik4 <laika@albina.cc>
2020-12-15 02:29:37 +0900

    Fix gmpy2

--- scripts/gmpy2.sh
+++ scripts/gmpy2.sh
@@ -5,9 +5,9 @@ TMPDIR=${TMPDIR:-${HOME}/tmp}
 mkdir -p ${TMPDIR}
 
 prerequisite() {
-  sudo apt update 
-  sudo apt install -y build-essential m4 gnulib libgmp-dev libmpfr-dev libmpc-dev
+  sudo apt-get update 
+  sudo apt-get install -y build-essential m4 gnulib libgmp-dev libmpfr-dev libmpc-dev
 }
 
 prerequisite
-python -m pip install gmpy2
+${HOME}/.pyenv/shims/python -m pip install gmpy2
