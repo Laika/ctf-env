ec184b49bbd6e82fa96fb4d325d06be2054a2870
Laik4 <laika@albina.cc>
2021-02-19 23:37:28 +0900

    Change URL

--- scripts/wireshark.sh
+++ scripts/wireshark.sh
@@ -1,16 +1,15 @@
 #!/bin/bash -e
 
-VERSION=3.4.3
-FILENAME=wireshark-${VERSION}.tar.xz
-INITIAL_PATH=$(pwd)
-TMPDIR=${TMPDIR:-/tmp}
+VERSION="3.4.3"
+FILENAME="wireshark-${VERSION}.tar.xz"
+INITIAL_PATH="$(pwd)"
+TMPDIR="${TMPDIR:-/tmp}"
 
-sudo apt-get update -y
+sudo apt-get update 
 sudo apt-get install -y axel cmake flex bison libgcrypt20-dev libssh-dev libpcap-dev libsystemd-dev qtbase5-dev qttools5-dev qtmultimedia5-dev libqt5svg5-dev
 
-
-echo [+] Install c-ares
-CARES_VERSION=1.17.1
+echo "[+] Install c-ares"
+CARES_VERSION="1.17.1"
 CARES="c-ares-${CARES_VERSION}"
 
 if [ ! -e ${TMPDIR}/${CARES} ]; then
@@ -25,19 +24,19 @@ make -j$(nproc) && sudo make install && rm -rf ${TMPDIR}/${CARES}
 
 
 
-echo [+] Download ${FILENAME}
+echo "[+] Download ${FILENAME}"
 if [ ! -e ${TMPDIR}/${FILENAME} ]; then
-    axel -a "https://1.eu.dl.wireshark.org/src/${FILENAME}" -o ${TMPDIR}
+    axel -a "https://2.na.dl.wireshark.org/src/${FILENAME}" -o ${TMPDIR}
 fi
 cd ${TMPDIR}
 tar xf ${FILENAME}
 mkdir -p ${TMPDIR}/build && cd ${TMPDIR}/build
 
-echo [+] Start building
+echo "[+] Start building"
 cmake ${TMPDIR}/wireshark-${VERSION}
 make -j$(nproc)
 sudo make install
 rm -rf ${TMPDIR}/build
 rm -f ${TMPDIR}/${FILENAME}*
-echo [+] Done
+echo "[+] Done"
 cd ${INITIAL_PATH}
