93e62f8dcc60a8cec979c04063b061929501dfba
Laik4 <laika@albina.cc>
2021-02-20 00:13:25 +0900

    Update burpsuite

--- scripts/burpsuite.sh
+++ scripts/burpsuite.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-INIT_PATH=$(pwd)
-TMPDIR=${TMPDIR:-/tmp}
-VERSION=2020.12.1
-FILENAME=burpsuite_community_linux_v${VERSION//\./\_}
+INITIAL_PATH="$(pwd)"
+TMPDIR="${TMPDIR:-/tmp}"
 
-sudo apt update -y
-sudo apt install -y axel
+_prerequisite(){
+  sudo apt update 
+  sudo apt install -y axel
+}
 
-if [ ! -e ${TMPDIR}/${FILENAME}.sh ]; then
-  axel -n 8 -v \
-    -a "https://portswigger.net/burp/releases/download?product=community&version=${VERSION}&type=Linux" \
-    -o ${TMPDIR}
-fi
+_burpsuite(){
+  local VERSION="2021.2.1"
+  FILENAME="burpsuite_community_linux_v${VERSION//\./\_}"
+  if [ ! -e ${TMPDIR}/${FILENAME}.sh ]; then
+    axel -n 8 -v \
+      -a "https://portswigger.net/burp/releases/download?product=community&version=${VERSION}&type=Linux" \
+      -o ${TMPDIR}
+  fi
 
-cd ${TMPDIR}
-bash ${FILENAME}.sh
-rm ${FILENAME}.sh
+  cd ${TMPDIR}
+  bash ${FILENAME}.sh
+}
+_postprocessing(){
+  rm ${FILENAME}.sh
 
-cd ${INIT_PATH}
+  cd ${INIT_PATH}
+}
+
+_prerequisite
+_burpsuite
+_postprocessing
