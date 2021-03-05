6e44af20acc9723ef24aa8cf040ec7a5d212bcb8
Laik4 <runner@fv-az180-942.42qyr4o3fwvefpb200thk0z2kh.bx.internal.cloudapp.net>
2021-03-05 06:23:17 +0000

    Fix scripts

--- scripts/stegsolve.sh
+++ scripts/stegsolve.sh
@@ -3,9 +3,9 @@
 TMPDIR="${TMPDIR:-/tmp}"
 
 _install(){
-  wget "http://www.caesum.com/handbook/Stegsolve.jar" -O ${TMPDIR}/stegsolve.jar
-  chmod +x ${TMPDIR}/stegsolve.jar
-  sudo mv ${TMPDIR}/stegsolve.jar /usr/local/bin
+  wget "http://www.caesum.com/handbook/Stegsolve.jar" -O ${TMPDIR}/stegsolve
+  chmod +x ${TMPDIR}/stegsolve
+  sudo mv ${TMPDIR}/stegsolve /usr/local/bin
 }
 
 if [ ! $(command -v stegsolve) ]; then
