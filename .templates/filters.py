0f0913dd56dddc77b1f74e27172c11a43439c214
Laik4 <laika@albina.cc>
2021-03-09 00:47:36 +0900

    Fix template

--- .templates/filters.py
+++ .templates/filters.py
@@ -1,22 +1,5 @@
 import urllib
 
 
-class DotDict(dict):
-    __getattr__ = dict.__getitem__
-    __setattr__ = dict.__setitem__
-    __delattr__ = dict.__delitem__
-
-    def __init__(self, dct):
-        for key, value in dct.items():
-            if hasattr(value, "keys"):
-                value = DotDict(value)
-
-            self[key] = value
-
-
-def dotdict(arg):
-    return DotDict(list(arg)[0])
-
-
 def urlencode(arg):
     return urllib.parse.quote(arg)
